7732a7113c85aef33aa8c97e2a0f560a54d99a7b
Roisin Stephens <roisinstephens@Roisins-MacBook-Pro.local>
2016-11-26 17:40:53 +0000

    new routes

--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,49 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/students', function(req, res, next) {
-	Student.find(function(err, students) {
+	var subjects = req.query.subjects;
+	var level = req.query.level;
+	var nationality = req.query.nationality;
+
+	var query = Student.find();
+	if (subjects){
+		query = query.where('subjects').in(subjects);
+	}
+	if (level) {
+		query = query.where('level').equals(level);
+	}
+	if (nationality) {
+		query = query.where('nationality').equals(nationality);
+	}
+
+	query.exec(function(err, students) {
 		if(err){return next(err);}
 		res.json(students);
 	});
 });
 
 router.get('/tutors', function(req, res, next) {
+	var subjects = req.query.subjects;
+	var level = req.query.level;
+	var nationality = req.query.nationality;
 
-	Tutor.find(function(err, tutors){
+	var query = Tutor.find();
+	if (subjects) {
+		query = query.where('subjects').in(subjects);
+	}
+	if (level) {
+		query = query.where('level').equals(level);
+	}
+	if (nationality) {
+		query = query.where('nationality').equals(nationality);
+	}
+
+	query.exec(function(err, tutors){
 		if(err){return next(err);}
 		res.json(tutors);
 	});
 });
+
 
 // expects that req.body has the correct fields set
 router.post('/addstudent', function(req, res, next) {
@@ -36,6 +66,7 @@ router.post('/addstudent', function(req, res, next) {
 
 // expects that req.body has the correct fields set
 router.post('/addtutor', function(req, res, next){
+	console.log("request body: " + req.body);
 	var tutorInput = new Tutor(req.body);
 	tutorInput.save(function(err, tutorInput){
 		if(err){return next(err);}
